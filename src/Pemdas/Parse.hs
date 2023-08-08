ff90122611d16746bd66599d2e0d3d8976f49b5d
Jonathan Daugherty <cygnus@foobox.com>
2023-08-08 13:28:54 -0700

    Parse.hs: add comments

--- src/Pemdas/Parse.hs
+++ src/Pemdas/Parse.hs
@@ -1,4 +1,12 @@
 module Pemdas.Parse
+-- JTD:
+--
+-- I noted something along these lines in Main.hs, but unless you want
+-- this module to export a parser specifically be composed with other
+-- Parsec parsers, it would probably be better to hide the parser
+-- entirely and just export a function parseMyStuff :: String -> Either
+-- Error Stuff. That keeps the parsing operation abstract and reduces
+-- API burden for the caller.
     (makeExprParser)
 where
 
@@ -26,12 +34,25 @@ makeExprParser binOps = expr
 
     term :: Parser (PT.Expr Double)
     term =
+        -- JTD:
+        --
+        -- Some of these alternatives are long enough that I recommend
+        -- pulling them out as top-level declarations. That will make
+        -- this very concise and easy to understand, and it will also
+        -- make reordering them (if that's ever deemed appropriate) a
+        -- matter of just moving a line or two around, and that will
+        -- result in smaller commits that are easier to understand.
         parens expr
             <|> simpleExpr
             <|> (PT.Negated <$> (char '-' >> expr))
             -- TODO this approach leads to bad error messages.
             -- Would be better to diverge later if we can
             <|> try (do
+                    -- JTD:
+                    --
+                    -- This can also be written
+                    --
+                    -- void $ char '\\'
                     _ <- char '\\'
                     aggName <- identifier'
                     _ <- char '_'
@@ -76,6 +97,9 @@ makeExprParser binOps = expr
     simpleExpr =
         (PT.Literal <$> literal)
         <|> (PT.Variable <$> identifier)
+    -- JTD:
+    --
+    -- The line after this one has trailing whitespace.
     
     precedenceTable :: [(String, Int)]
     precedenceTable = [(opText, prec) | (opText, (prec, _)) <- binOps]
@@ -87,7 +111,15 @@ makeExprParser binOps = expr
             many $ do
                 op <- operator
                 nextTerm <- term
+                -- JTD:
+                --
+                -- I suggest pulling this triple out and giving it a let-bound name.
                 return
+                    -- JTD:
+                    --
+                    -- Equivalent to
+                    --
+                    -- fromMaybe (-1) $ lookup op precedenceTable
                     ( case lookup op precedenceTable of
                         Just n -> n
                         Nothing -> -1
